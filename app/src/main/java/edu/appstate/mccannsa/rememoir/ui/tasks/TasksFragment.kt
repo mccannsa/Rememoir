9b16f28c14fac19d8d386eb1ce76c108e0f78896
mccannsa <mccannsa@appstate.edu>
2021-02-21 19:35:50 -0500

    connected Firebase to Rememoir

--- app/src/main/java/edu/appstate/mccannsa/rememoir/ui/tasks/TasksFragment.kt
+++ app/src/main/java/edu/appstate/mccannsa/rememoir/ui/tasks/TasksFragment.kt
@@ -4,24 +4,30 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Button
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import edu.appstate.mccannsa.rememoir.R
 
 class TasksFragment : Fragment() {
 
-    private lateinit var dashboardViewModel: TasksViewModel
+    private lateinit var tasksViewModel: TasksViewModel
+    private val db = Firebase.firestore
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        dashboardViewModel =
+        tasksViewModel =
                 ViewModelProvider(this).get(TasksViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_tasks, container, false)
+        val addButton = container?.findViewById(R.id.addButton) as Button
+        addButton.setOnClickListener {
+            
+        }
         return root
     }
 }
