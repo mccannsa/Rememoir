1e90dbf81fca4500896fe1b22b95355e45f6604c
mccannsa <mccannsa@appstate.edu>
2021-02-23 15:55:54 -0500

    tasks now populate task view, update Firebase when checked

--- app/src/main/java/edu/appstate/mccannsa/rememoir/ui/tasks/TasksFragment.kt
+++ app/src/main/java/edu/appstate/mccannsa/rememoir/ui/tasks/TasksFragment.kt
@@ -5,7 +5,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import android.widget.*
+import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.google.firebase.firestore.ktx.firestore
@@ -17,6 +18,7 @@ import edu.appstate.mccannsa.rememoir.R
 class TasksFragment : Fragment() {
 
     private lateinit var tasksViewModel: TasksViewModel
+    private lateinit var linearLayout: LinearLayout
     private lateinit var createTaskButton: Button
     private val db = Firebase.firestore
 
@@ -28,11 +30,38 @@ class TasksFragment : Fragment() {
         tasksViewModel =
                 ViewModelProvider(this).get(TasksViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_tasks, container, false)
+
         createTaskButton = root.findViewById(R.id.buttonCreateTask)
         createTaskButton.setOnClickListener {
             val showCreateTask = Intent(activity, CreateTaskActivity::class.java)
             startActivity(showCreateTask)
         }
+
+        // pull tasks from db and populate view
+        linearLayout = root.findViewById(R.id.taskLinearLayout)
+        db.collection("tasks")
+                .get()
+                .addOnSuccessListener { result ->
+                    for (document in result) {
+                        val name = document.data.get("name") as String
+                        val checked = document.data.get("checked") as Boolean
+
+                        val card = CardView(requireContext())
+                        card.radius = 15f
+                        card.setPadding(25, 25, 25, 25)
+
+                        val checkBox = CheckBox(requireContext())
+                        checkBox.text = name
+                        checkBox.isChecked = checked
+
+                        checkBox.setOnClickListener {
+                            document.reference.update("checked", checkBox.isChecked)
+                        }
+
+                        card.addView(checkBox)
+                        linearLayout.addView(card)
+                    }
+                }
         return root
     }
 }
