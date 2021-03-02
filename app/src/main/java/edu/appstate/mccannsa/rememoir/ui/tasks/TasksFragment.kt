cf9b225c23d4f221345cbb1bbd75553aa3e43284
mccannsa <mccannsa@appstate.edu>
2021-03-02 16:53:49 -0500

    formatting cards; added time to tasks in task list

--- app/src/main/java/edu/appstate/mccannsa/rememoir/ui/tasks/TasksFragment.kt
+++ app/src/main/java/edu/appstate/mccannsa/rememoir/ui/tasks/TasksFragment.kt
@@ -1,18 +1,24 @@
 package edu.appstate.mccannsa.rememoir.ui.tasks
 
 import android.content.Intent
+import android.content.res.ColorStateList
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
 import androidx.cardview.widget.CardView
+import androidx.core.view.setPadding
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.google.firebase.Timestamp
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import edu.appstate.mccannsa.rememoir.CreateTaskActivity
 import edu.appstate.mccannsa.rememoir.R
+import java.text.SimpleDateFormat
+import java.util.*
 
 class TasksFragment : Fragment() {
 
@@ -41,23 +47,35 @@ class TasksFragment : Fragment() {
         db.collection("tasks")
                 .get()
                 .addOnSuccessListener { result ->
+
+                    val dateFormat = SimpleDateFormat("MMMMM dd h:mm a")
+
                     for (document in result) {
+
                         val name = document.data.get("name") as String
+                        val taskDateTime = document.data.get("dateTime") as Timestamp
                         val checked = document.data.get("checked") as Boolean
 
                         val card = CardView(requireContext())
                         card.radius = 15f
                         card.setPadding(25, 25, 25, 25)
+                        card.setCardBackgroundColor(Color.LTGRAY)
 
                         val checkBox = CheckBox(requireContext())
                         checkBox.text = name
+                        checkBox.textSize = 20f
                         checkBox.isChecked = checked
 
                         checkBox.setOnClickListener {
                             document.reference.update("checked", checkBox.isChecked)
                         }
 
+                        val tv = TextView(requireContext())
+                        tv.text = taskDateTime.toDate().toString()
+                        tv.setPadding(50)
+
                         card.addView(checkBox)
+                        card.addView(tv)
                         linearLayout.addView(card)
                     }
                 }
