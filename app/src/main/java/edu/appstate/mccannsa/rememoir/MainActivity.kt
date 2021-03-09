6b96a4005f8e78e78086724df5ee46d8ee23d5be
mccannsa <mccannsa@appstate.edu>
2021-03-09 13:02:37 -0500

    beginning work on task notifications

--- app/src/main/java/edu/appstate/mccannsa/rememoir/MainActivity.kt
+++ app/src/main/java/edu/appstate/mccannsa/rememoir/MainActivity.kt
@@ -1,5 +1,9 @@
 package edu.appstate.mccannsa.rememoir
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
@@ -10,6 +14,7 @@ import androidx.navigation.ui.setupWithNavController
 
 class MainActivity : AppCompatActivity() {
 
+    val CHANNEL_ID = "rememoir"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -22,5 +27,23 @@ class MainActivity : AppCompatActivity() {
                 R.id.navigation_home, R.id.navigation_tasks, R.id.navigation_journal))
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+    }
+
+    private fun createNotificationChannel() {
+        // Create notification channel if on API 26 or higher
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
+            val name = "rememoir_channel"
+            val descriptionText = "Notification channel for Rememoir app"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+                description = descriptionText
+            }
+
+            // Register the channel with the system
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
     }
 }
